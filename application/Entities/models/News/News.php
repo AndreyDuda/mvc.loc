5365d2f367c46f0c7624e5d7de201eadbdeecbf0
AndreyDuda <AndreyDuda88@gmail.com>
2019-06-17 14:41:51 +0300

    add view

--- application/Entities/models/News/News.php
+++ application/Entities/models/News/News.php
@@ -22,7 +22,22 @@ class News
 	public static function getNewsList()
 	{
 		
+		/*
+		$sqlset = "SET NAMES utf8 SET collation_connection = 'utf8_general_ci'";
+		$sql = "SHOW SESSION VARIABLES LIKE 'character_set_connection'";
+		$db = (new Connection())->queryBuilder()
+			->query($sqlset);
+		$db = (new Connection())->queryBuilder()
+			->query($sql);
+		$res = $db->fetch();
+		print_r($res);
+		echo "<hr/>";
+		$sql = "SHOW SESSION VARIABLES LIKE 'collation_connection'";
+		$res = mysql_query($sql);
+		print_r(mysql_result($res, 0, 1));
+		echo "<hr/>";
 		
+		die;*/
 		$db = (new Connection())->queryBuilder()
 			->query(
 			'SELECT `id`, `title`, `date`, `short_content`
---
+++ public/views/news/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Effloresce  by FCT</title>
+<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+<link href="/recource/assets/css/style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+	<div id="menu-wrapper">
+		<div id="menu">
+			<ul>
+				<li class="current_page_item"><a href="#">Homepage</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Photos</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Links</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+		<!-- end #menu -->
+	</div>
+
+<div id="wrapper">
+	<div id="header-wrapper">
+		<div id="header">
+			<div id="logo">
+				<h1><a href="#">Effloresce </a></h1>
+				<p>recource Шаблоны для сайта <a href="http://www.frecource.ru/">СКАЧАТЬ</a></p>
+			</div>
+		</div>
+	</div>
+	<!-- end #header -->
+	<div id="page">
+		<div id="page-bgtop">
+			<div id="page-bgbtm">
+				<div id="content">
+					<?php foreach ($newsList as $newsItem):?>
+					<div class="post">
+						<h2 class="title"><a href='/news/<?php echo $newsItem['id'] ;?>'><?php echo $newsItem['title'].' # '.$newsItem['id'];?></a></h2>
+						<p class="meta">Posted by <a href="#"><?php echo $newsItem['id'];?></a> on <?php echo $newsItem['date'];?>
+							&nbsp;&bull;&nbsp; <a href='/news/<?php echo $newsItem['id'] ;?>' class="permalink"> Full article</a></p>
+						<div class="entry">
+							<p><img src="/recource/images/pic01.jpg" width="800" height="300" alt="" /></p>
+							<p><?php echo $newsItem['short_content'];?></p>
+						</div>
+					</div>
+				<?php endforeach;?>
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+				<!-- end #content -->
+				<div id="sidebar">
+					<ul>
+						<li>
+							<h2>Aliquam tempus</h2>
+							<p>Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper consectetuer hendrerit.</p>
+						</li>
+						<li>
+							<h2>Categories</h2>
+							<ul>
+								<li><a href="#">Aliquam libero</a></li>
+								<li><a href="#">Consectetuer adipiscing elit</a></li>
+								<li><a href="#">Metus aliquam pellentesque</a></li>
+								<li><a href="#">Suspendisse iaculis mauris</a></li>
+								<li><a href="#">Urnanet non molestie semper</a></li>
+								<li><a href="#">Proin gravida orci porttitor</a></li>
+							</ul>
+						</li>
+						<li>
+							<h2>Blogroll</h2>
+							<ul>
+								<li><a href="#">Aliquam libero</a></li>
+								<li><a href="#">Consectetuer adipiscing elit</a></li>
+								<li><a href="#">Metus aliquam pellentesque</a></li>
+								<li><a href="#">Suspendisse iaculis mauris</a></li>
+								<li><a href="#">Urnanet non molestie semper</a></li>
+								<li><a href="#">Proin gravida orci porttitor</a></li>
+							</ul>
+						</li>
+						<li>
+							<h2>Archives</h2>
+							<ul>
+								<li><a href="#">Aliquam libero</a></li>
+								<li><a href="#">Consectetuer adipiscing elit</a></li>
+								<li><a href="#">Metus aliquam pellentesque</a></li>
+								<li><a href="#">Suspendisse iaculis mauris</a></li>
+								<li><a href="#">Urnanet non molestie semper</a></li>
+								<li><a href="#">Proin gravida orci porttitor</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<!-- end #sidebar -->
+				<div style="clear: both;">&nbsp;</div>
+			</div>
+		</div>
+	</div>
+	<!-- end #page -->
+</div>
+<div id="footer">
+	<p>Copyright (c) 2013 Sitename.com. All rights reserved. Design by <a href="http://www.freecssrecources.org">FCT</a>. Photos by Fotogrph</a>.</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
